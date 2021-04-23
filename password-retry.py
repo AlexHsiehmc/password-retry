d5cf8b27fa077cd636383d2328990da5b2eb159b
Alex Hsieh <alex174hmc@gmail.com>
2021-04-24 01:36:37 +0800

    simplify code version

--- password-retry.py
+++ password-retry.py
@@ -22,11 +22,13 @@
 password = 'a123456'
 x = 3 # 剩餘機會
 while x > 0:
+	x = x - 1
 	p = input('請輸入密碼:')
 	if p == password:
 		print('登入成功!')
 		break
 
 	else:
-		x = x - 1
-		print('密碼錯誤! 還有', x, '次機會')
+		print('密碼錯誤!')
+		if x > 0:
+			print('還有', x, '次機會')
