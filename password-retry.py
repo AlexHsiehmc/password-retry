da8a3f53b01f38f0f8ed5695b63282e83d5637e8
Alex Hsieh <alex174hmc@gmail.com>
2021-04-24 01:23:33 +0800

    basic version done

---
+++ password-retry.py
@@ -0,0 +1,20 @@
+# 密碼重試程式
+# password = 'a123456'
+# 讓使用者重複輸入密碼
+# 最多輸入3次
+# 如果正確 就印出 "登入成功!"
+# 如果不正確 就印出 "密碼錯誤! 還有_次機會!"
+
+password = 'a123456'
+x = 3 # 剩餘機會
+while True:
+	p = input('請輸入密碼:')
+	if p == password:
+		print('登入成功!')
+		break
+
+	else:
+		x = x - 1
+		print('密碼錯誤! 還有', x, '次機會')
+		if x == 0:
+			break
